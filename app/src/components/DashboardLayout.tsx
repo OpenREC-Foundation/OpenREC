c474718d84ba8d0f1e087df6a7f662a255cae0e8
Davi <languith54@gmail.com>
2026-07-15 16:23:25 -0300

    update app/src/components/DashboardLayout.tsx

---
+++ app/src/components/DashboardLayout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../shared/Navbar';
+import Sidebar from '../shared/Sidebar';
+
+const DashboardLayout = () => {
+  return (
+    <div className="flex h-screen bg-gray-950 text-white overflow-hidden">
+      <Sidebar />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <Navbar />
+        <main className="flex-1 overflow-y-auto p-6 space-y-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
