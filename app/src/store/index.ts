aaec4d59b5847250d5e515291cf565173ddc778b
Davi <languith54@gmail.com>
2026-07-15 17:16:06 -0300

    Create index.ts to export store slices

---
+++ app/src/store/index.ts
@@ -0,0 +1,6 @@
+export { useRecorderStore } from './recorderSlice';
+export { useEditorStore } from './editorSlice';
+export { useProjectStore } from './projectSlice';
+export { useAIStore } from './aiSlice';
+export { usePacksStore } from './packsSlice';
+export { usePluginsStore } from './pluginsSlice';
